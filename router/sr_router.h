4176404e97d81d2c880c6e41d49d73ccaeac9913
MTrann <34174699+MTrann@users.noreply.github.com>
2017-12-03 21:22:11 -0600

    Update sr_router.h

--- router/sr_router.h
+++ router/sr_router.h
@@ -66,7 +66,7 @@ int sr_read_from_server(struct sr_instance* );
 
 /* -- sr_router.c -- */
 void sr_init(struct sr_instance* );
-void sr_handlepacket(struct sr_instance* , uint8_t * , unsigned int , char* );
+/*void sr_handlepacket(struct sr_instance* , uint8_t * , unsigned int , char* );*/
 void sr_handle_arpreq(struct sr_instance *sr, struct sr_arpreq *req,
     struct sr_if *out_iface);
 
@@ -75,5 +75,19 @@ void sr_add_interface(struct sr_instance* , const char* );
 void sr_set_ether_ip(struct sr_instance* , uint32_t );
 void sr_set_ether_addr(struct sr_instance* , const unsigned char* );
 void sr_print_if_list(struct sr_instance* );
+
+void sr_handlepacket(struct sr_instance* sr,uint8_t * packet/* lent */,unsigned int len, char* interface/* lent */);
+void sr_waitforarp(struct sr_instance *sr, uint8_t *pkt,unsigned int len, uint32_t next_hop_ip, struct sr_if *out_iface);
+void sr_handlepacket_ip(struct sr_instance* sr,uint8_t *packet,unsigned int len,char* interface);
+void sr_forward_handler(struct sr_instance* sr,uint8_t *packet,unsigned int len,sr_if *interface);
+void sr_forward_packet(struct sr_instance* sr,uint8_t *packet,unsigned int len,uint8_t macAddress,struct  sr_if *outgoingInterface);
+void sr_handle_ip_packet_reception(struct sr_instance* sr,uint8_t *packet,unsigned int len,sr_if *interface);
+uint8_t is_ip_packet_ok(sr_ip_hdr_t *ip_header,unsigned int len);
+uint8_t is_icmp_packet_ok(sr_icmp_hdr_t *icmp_header,unsigned int len);
+uint8_t is_icmp_chksum_ok(sr_icmp_hdr_t *icmp_header);
+uint8_t is_ip_chksum_ok(sr_ip_hdr_t *ip_header);
+int send_icmp(struct sr_instance* sr, uint8_t icmp_type, uint8_t icmp_code,uint8_t *originalPacket, struct sr_if *interface);
+
+
 
 #endif /* SR_ROUTER_H */
