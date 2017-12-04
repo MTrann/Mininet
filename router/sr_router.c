855ec965122fb4eced099b03628acd20a22cf543
Matime <matthewmerlene@gmail.com>
2017-12-03 22:00:57 -0600

    fixed header and other

--- router/sr_router.c
+++ router/sr_router.c
@@ -179,7 +179,7 @@ void sr_handle_arpreq(struct sr_instance *sr, struct sr_arpreq *req,
      char*  interface;
      while(curr_packet != NULL){
          printf("Send ICMP host unreachable packet");
-         send_icmp(sr,0x0003,0x0003,packets,interface);
+         send_icmp(sr,0x0003,0x0003,packets,out_iface);
 /*>>>>>>> aedcca6dca9182fb330dc30deb7857051e3d18e2*/
          curr_packet = curr_packet -> next;
      }
@@ -319,7 +319,7 @@ void sr_handlepacket(struct sr_instance* sr,
       sr_handlepacket_arp(sr,packet,len,simpleRouterInterface);
       break;
     default:
-      fprintf(stderr, "Packet type Unknown, dropping.", );
+      fprintf(stderr, "Packet type Unknown, dropping.");
       
   }
 }
